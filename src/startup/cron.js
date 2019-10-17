26f9942dc3f1da026bd2191fa51b755d5e2c34ae
Vasilisa <vmedvedenko@lohika.com>
2019-10-17 11:53:10 +0300

    changes cron schedule to 1 hour

--- src/startup/cron.js
+++ src/startup/cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const logger = require('../logging');
 const {updateSourcesData} = require("../handlers/updateData");
 
-cron.schedule("0 * * * * *", async function () {
+cron.schedule("0 0 1 * * *", async function () {
   await updateSourcesData();
   logger.info('Cron update ', (new Date).toISOString());
 });
