bbc9c7ba80ed413ae9fe54baf093e49acaafcb5c
Vasilisa <vmedvedenko@lohika.com>
2019-10-23 16:51:55 +0300

    fixed token validation

--- src/routes/trigger.route.js
+++ src/routes/trigger.route.js
@@ -4,19 +4,24 @@ const router = express.Router();
 const {handleTrigger} = require('../handlers/trigger');
 
 router.post('/', async (req, res) => {
+  if (!isTokenCorrect(req.body)) {
+    return res.status(400).send('Check authenticationToken');
+  }
+
   try {
-    if (!req.body.authenticationToken || req.body.authenticationToken.length < 32) {
-      res.status(400).send('Check authenticationToken');
-    }
     const result = await handleTrigger(req.body);
     return res.status(200).send({result: 'OK'});
   } catch (error) {
     logger.error('Error Trigger Res: ', error);
-    if(error.code){
+    if (error.code) {
       return res.status(error.code).send(error.msg);
     }
     res.status(500).send('Something broke!');
   }
 });
+
+function isTokenCorrect(body) {
+  return body.sourceUpdate && body.sourceUpdate.authenticationToken && body.sourceUpdate.authenticationToken.length > 31;
+}
 
 module.exports = router;
