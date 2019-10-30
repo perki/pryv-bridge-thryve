8572d14160e5307d41c8b3c19218dfa37a8289db
Vasilisa <vmedvedenko@lohika.com>
2019-10-30 20:33:54 +0200

    handled 404 error

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('./startup/db');
 require('./startup/cron');
+
 const express = require('express');
 const config = require('./config.js');
 const routes = require('./routes');
@@ -9,23 +10,11 @@ app.use(require('body-parser').json());
 app.use('/user', routes.user);
 app.use('/trigger', routes.trigger);
 
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
-/**
- * Not used yet
- */
-app.post('/auto', async (req, res) => {
-  try {
-    // storage.addSyncSourceForUser(req.body.pryvEndpoint, req.body.source); // todo need to be moved to handlers
-    res.status(200).send({result: 'OK'});
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+app.use((req, res) => res.status(404).end());
 
 const port = config.get('server:port');
-
 app.listen(port, () => console.log(`${(new Date()).toISOString()}: Thryve <> Pryv bridge listening on port ${port}!`));
 
 
