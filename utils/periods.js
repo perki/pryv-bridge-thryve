f4f45969920087385ecc685a06468d668e1b1837
Volodymyr Chukhalonok <vchukhalonok@lohika.com>
2020-04-06 16:22:58 +0300

    Update periods.js

--- utils/periods.js
+++ utils/periods.js
@@ -5,23 +5,21 @@ const {
 } = require('date-fns');
 const config = require('../config');
 
-const currentDate = new Date();
-
 exports.getCurrentDate = function() {
-    return currentDate;
+    return new Date();
 };
 
 exports.getCurrentDateTimestamp = function() {
-    return getUnixTime(currentDate);
+    return getUnixTime(getCurrentDate());
 };
 
 exports.getPeriodAgoTimestamp = function () {
-    const dayAgoDate = addDays(currentDate, config.get('cron:period'));
+    const dayAgoDate = addDays(getCurrentDate(), config.get('cron:period'));
     return getUnixTime(dayAgoDate);
 };
 
 exports.getPeriodAgo = function() {
-    return addDays(currentDate, config.get('cron:period'));
+    return addDays(getCurrentDate(), config.get('cron:period'));
 };
 
 exports.tsToDate = function (ts) {
